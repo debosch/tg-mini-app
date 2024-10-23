7b23309a1d4ed2e764144869d008d15cf2caa106
debosch <vlad.shiltsin@gmail.com>
2024-10-23 18:13:44 +0200

    Remove redundant assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
 import eruda from 'eruda'
@@ -8,29 +5,11 @@ import eruda from 'eruda'
 eruda.init()
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+        <button onClick={() => WebApp.showAlert("Alert!")}>
             Show Alert
         </button>
-      </div>
     </>
   )
 }
