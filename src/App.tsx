46aeac70acb719e5a7418a9a5d655545e609b650
debosch <vlad.shiltsin@gmail.com>
2024-10-23 19:23:27 +0200

    Add log with user info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import WebApp from '@twa-dev/sdk'
 import eruda from 'eruda'
+import { useEffect } from 'react'
 
 eruda.init()
 
 function App() {
+  useEffect(() => {
+    console.log(WebApp.initDataUnsafe.user);
+  }, []);
+
   return (
     <>
         <button onClick={() => WebApp.showAlert("Alert!")}>
