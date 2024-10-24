7dad3d231e6df82ce2256ba8e2953d64506813de
debosch <vlad.shiltsin@gmail.com>
2024-10-24 16:08:34 +0200

    Add button to scan qr

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,11 @@ function App() {
         <button onClick={() => WebApp.showAlert("Alert!")}>
             Show Alert
         </button>
+        <br />
+        <br />
+        <button onClick={() => WebApp.showScanQrPopup({text: ''})}>
+          Scan QR
+        </button>
     </>
   )
 }
